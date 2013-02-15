abd2a593749e217fdf95843ba0bac513ae61a91b
Patrick Walton <pcwalton@mimiga.net>
2013-02-14 19:13:56 -0800

    Start work on new DOM representation

--- src/servo/css/matching.rs
+++ src/servo/css/matching.rs
@@ -2,7 +2,7 @@
 
 use css::node_util::NodeUtil;
 use css::select_handler::NodeSelectHandler;
-use dom::node::{Node, NodeTree};
+use dom::node::AbstractNode;
 use layout::context::LayoutContext;
 use newcss::complete::CompleteSelectResults;
 use newcss::select::{SelectCtx, SelectResults};
@@ -10,60 +10,48 @@ use newcss::select::{SelectCtx, SelectResults};
 use std::arc::{ARC, get, clone};
 
 pub trait MatchMethods {
-    fn restyle_subtree(select_ctx: &SelectCtx);
+    fn restyle_subtree(&self, select_ctx: &SelectCtx);
 }
 
-impl Node : MatchMethods {
+impl MatchMethods for AbstractNode {
     /**
      * Performs CSS selector matching on a subtree.
-
+     *
      * This is, importantly, the function that updates the layout data for
      * the node (the reader-auxiliary box in the COW model) with the
      * computed style.
      */
-    fn restyle_subtree(select_ctx: &SelectCtx) {
-
+    fn restyle_subtree(&self, select_ctx: &SelectCtx) {
         // Only elements have styles
         if self.is_element() {
-            let select_handler = NodeSelectHandler {
-                node: self
-            };
-            let incomplete_results = select_ctx.select_style(&self, &select_handler);
+            let select_handler = NodeSelectHandler { node: *self };
+            let incomplete_results = select_ctx.select_style(self, &select_handler);
             // Combine this node's results with its parent's to resolve all inherited values
-            let complete_results = compose_results(&self, move incomplete_results);
-            self.set_css_select_results(move complete_results);
+            let complete_results = compose_results(*self, incomplete_results);
+            self.set_css_select_results(complete_results);
         }
 
-        let mut i = 0u;
-        
-        for NodeTree.each_child(&self) |kid| {
-            i = i + 1u;
+        for self.each_child |kid| {
             kid.restyle_subtree(select_ctx); 
         }
     }
 }
 
-fn compose_results(node: &Node, results: SelectResults) -> CompleteSelectResults {
+fn compose_results(node: AbstractNode, results: SelectResults) -> CompleteSelectResults {
     match find_parent_element_node(node) {
-        None => CompleteSelectResults::new_root(move results),
+        None => CompleteSelectResults::new_root(results),
         Some(parent_node) => {
             let parent_results = parent_node.get_css_select_results();
-            CompleteSelectResults::new_from_parent(parent_results, move results)
+            CompleteSelectResults::new_from_parent(parent_results, results)
         }
     }    
 }
 
-fn find_parent_element_node(node: &Node) -> Option<Node> {
-    use util::tree::parent;
-
-    match parent(&NodeTree, node) {
-        Some(ref parent) => {
-            if parent.is_element() {
-                Some(*parent)
-            } else {
-                find_parent_element_node(parent)
-            }
-        }
-        None => None
+fn find_parent_element_node(node: AbstractNode) -> Option<AbstractNode> {
+    match node.parent_node() {
+        Some(parent) if parent.is_element() => Some(parent),
+        Some(parent) => find_parent_element_node(parent),
+        None => None,
     }
 }
+
